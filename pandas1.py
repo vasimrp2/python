608945dc096c57bdc6f8c1976a6684cb267f87d6
Vasim Pathan <vasimrp2@gmail.com>
2026-04-08 15:35:22 +0530

    Pandas Basic

---
+++ pandas1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David'],  
+    'Age': [25, 30, 35, 40],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+}
+
+for i in range(len(data['Name'])):
+    print(f"Name: {data['Name'][i]}, Age: {data['Age'][i]}, City: {data['City'][i]}")
